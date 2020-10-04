d4e57ce8ffc39726a7ccfcfcea28199f2832dc1f
BernardinDEV <65625433+BernardinDEV@users.noreply.github.com>
2020-10-04 10:06:51 -0300

    Add files via upload

---
+++ Lista.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Text;
+using System.IO;
+
+namespace Trabalho
+{
+    public class Lista
+    {
+
+        public Node head;
+        public Node tail;
+        public Node contato;
+
+
+        public Lista()
+        {
+            head = null;
+
+        }
+
+        // Crud
+
+        public void addList(string nome, string email, string numero)
+        {
+            var newNode = new Node(nome, email, numero);
+            Node aux = head;
+            if (head != null)
+            {
+                while (aux.next != head)
+                {
+                    aux = aux.next;
+                }
+
+
+            } else
+            {
+                aux = newNode;
+            }
+            newNode.next = head;
+            head = newNode;
+            aux.next = head;
+           
+        }
+        public void removeList(string achar)
+        {
+            if (head == null)
+            {
+                Console.Write("Lista Vazia.");
+            }
+            Node aux = head;
+            Node ant = null;
+            if (achar == head.nome && head.next == head)
+            {
+                head = null;
+                Console.WriteLine("Contato removido");
+
+            }
+            else if(achar == head.nome && head.next != head)
+            {
+                head = head.next;
+                head.next = head;
+                
+
+                Console.WriteLine("Contato removido");
+
+            }
+
+            else
+            {
+                
+                while (aux.nome != achar && ant != head)
+                {
+                    ant = aux;
+                    aux = aux.next;
+
+                }
+                if (achar == aux.nome)
+                {
+                    ant.next = aux.next;
+                    Console.WriteLine("Contato removido");
+                }
+                else {
+                    Console.WriteLine("Contato nao encontrado");
+                }
+
+            }
+            
+
+
+        }
+        public void update(string nome, string email, string numero, string achar)
+        {
+           var aux2 = encontar(achar);
+            if (aux2.nome == achar)
+            {
+                aux2.nome = nome;
+                aux2.numero = numero;
+                aux2.email = email;
+                Console.WriteLine("Contato atualizado com sucesso");
+            }
+            else {
+                Console.WriteLine("Nao encontrado");
+            }
+
+        }
+        public  Node encontar(string nome)
+        {
+            if (head == null)
+            {
+                Console.Write("Lista Vazia.");
+            }
+            var aux = head;
+            if (aux.nome == nome)
+            {
+                Console.WriteLine($"-> [{aux.nome},{aux.email},{aux.numero}]");
+                return aux;
+            }
+           
+            
+                aux = aux.next;
+                while (aux.nome != nome && aux == head)
+                {
+                    aux = aux.next;
+                }
+                if (aux.nome == nome)
+                {
+                    Console.WriteLine($"-> [{aux.nome},{aux.email},{aux.numero}");
+                    return aux;
+                }
+                else
+                {
+                    Console.WriteLine("Contato nao encontrado");
+                    return head;
+                }
+
+            
+
+        }
+        //print
+        public void printList()
+        {
+
+            if (head == null)
+            {
+                Console.Write("Lista Vazia.");
+            }
+            Console.Write("[HEAD]");
+            var aux = head;
+            try
+            {
+                do
+                {
+                    Console.Write($"-> [{aux.nome},{aux.email},{aux.numero}]");
+                    aux = aux.next;
+                } while (aux != head);
+
+                Console.Write("-> [HEAD]");
+            }
+            catch
+            {
+                Console.WriteLine("\nErro");
+            }
+            }
+        // salvar 
+        public void salvarEmAqruivo()
+        {
+            if (head == null)
+            {
+                Console.Write("Lista Vazia.");
+            }
+            using (System.IO.StreamWriter sw = new System.IO.StreamWriter("text", true))
+                try
+            {
+                
+               
+                var aux = head;
+
+                do
+                {
+                    sw.Write($"-> [{aux.nome},{aux.email},{aux.numero}]");
+                    aux = aux.next;
+                } while (aux != head);
+
+                sw.Write("-> [HEAD]");
+                Console.WriteLine("Gravado com sucesso");
+
+               
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+           // Ordenarçao
+        }
+        public void ordenarNome()
+        {
+            if (head == null)
+            {
+                Console.Write("Lista Vazia.");
+            }
+
+        }
+        public void ordenarEmail()
+        {
+            if (head == null)
+            {
+                Console.Write("Lista Vazia.");
+            }
+            List <string> ls = new List <string>();
+            var aux = head;
+            do
+            {
+               
+                Console.Write($"-> [{aux.nome},{aux.email},{aux.numero}]");
+                ls.Add(aux.nome);
+                
+                
+                aux = aux.next;
+            } while (aux != head);
+
+            ls.Sort();
+
+        }
+        public void navegaçao()
+        {
+
+            if (head == null)
+            {
+                Console.Write("Lista Vazia.");
+            }
+            Console.Write($"-> [{head.nome},{head.email},{head.numero}]");
+            head = head.next;
+
+        }
+      
+
+
+
+
+    } 
+        
+}
---
+++ Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trabalho
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Lista circular = new Lista();
+            string w = Console.ReadLine();
+
+            do
+            {
+               
+                Console.Write("\nLista telefonica \n");
+                Console.Write("selecione uma opçao \n");
+                Console.Write("1->Adicionar um contato\n");
+                Console.Write("2->Remover um contato\n");
+                Console.Write("3->Atualizar um contato\n");
+                Console.Write("4->Encontar contato\n");
+                Console.Write("5->Mostar a lista\n");
+                Console.Write("6->Salvar a lista em Arquivo\n");
+                Console.Write("7->Ordenar agenda Por nome\n");
+                Console.Write("8->Ordernar agenda por Email\n");
+                Console.Write("9->Navegar Pela agenda\n");
+                string v = Console.ReadLine();
+                switch (v)
+
+                {
+                    case "1":
+                        Console.Clear();
+                        Console.WriteLine("Digite as seguintes informaçoes\n");
+                        Console.WriteLine("Nome:"); string nome = Console.ReadLine();
+                        Console.WriteLine("\nEmail:"); string email = Console.ReadLine();
+                        Console.WriteLine("\nNumero:"); string numero = Console.ReadLine();
+                        circular.addList(nome, email, numero);
+                        break;
+                    case "2":
+                        Console.WriteLine("Digite as seguintes informaçoes\n");
+                        Console.WriteLine("Qual contato deseja remover :"); string achar2 = Console.ReadLine();
+                        circular.removeList(achar2);
+
+                        break;
+                    case "3":
+                        Console.WriteLine("Digite as seguintes informaçoes\n");
+                        Console.WriteLine("Qual contato deseja atualizar :"); string achar1 = Console.ReadLine();
+                        Console.WriteLine("atualize o contato");
+                        Console.WriteLine("Nome:"); string nome1 = Console.ReadLine();
+                        Console.WriteLine("\nEmail:"); string email1 = Console.ReadLine();
+                        Console.WriteLine("\nNumero:"); string numero1 = Console.ReadLine();
+                        circular.update(nome1, email1, numero1, achar1);
+
+                        break;
+                    case "4":
+                        Console.WriteLine("digite as seguintes informaçoes\n");
+                        Console.WriteLine("Nome:"); string achar = Console.ReadLine();
+                        circular.encontar(achar);
+                        break;
+                    case "5":
+                        Console.WriteLine("Sua Lista");
+                        circular.printList();
+                        break;
+                    case "6":
+                        circular.salvarEmAqruivo();
+                        break;
+                    case "7":
+                        circular.ordenarNome();
+                        break;
+                    case "8":
+                        circular.ordenarEmail();
+                        break;
+                    case "9":
+                        var t = "0";
+                        do
+                        {
+                            Console.WriteLine("\nNavegar 'n' Sair 's':\n"); string navegar = Console.ReadLine();
+
+                            switch (navegar)
+                            {
+                                case "n":
+                                    circular.navegaçao();
+                                    break;
+                                case "s":
+                                    t = "1";
+                                    break;
+                            }
+                        } while (t == "0");
+                        break;
+                    case "Exit":
+                        w = "Exit";
+                        break;
+                }
+                
+
+            } while (w != "Exit");
+
+
+
+        }
+    }
+}
